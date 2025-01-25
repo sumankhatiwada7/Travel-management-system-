861efe931f18b3ace0e1c8190c68f0dfac654380
suman khatiwada Github <you@example.com>
2025-01-25 21:38:13 +0545

    done cab section

--- project.cpp
+++ project.cpp
@@ -36,6 +36,51 @@ class Customer{
 		}
 };
 class Cabs{
+	private:
+		int cabchoice;
+		int km;
+		float cost;
+		float lastcost;
+	public:
+		void cabdetails(){
+			
+		cout << "We collaborated with fastest, safest, and smartest cab service arround the country" << endl;
+        cout << "-----------ABC Cabs-----------\n" << endl;
+        cout << "1. Rent a Standard Cab - Rs.15 for 1KM" << endl;
+        cout << "2. Rent a Luxury Cab - Rs.25 per 1KM" << endl;
+
+        cout << "\nEnter another key to back or," << endl;
+
+        cout << "\nTo calculate the cost for your journey.." << endl;
+        cout << "Enter which kind of cab you need: ";
+        cin >> cabChoice;
+        cout << "Enter Kilometers you have to travel: ";
+        cin >> kilometers;
+        int hirecab;
+        if(cabchoice==1){
+        	cost=km*15;
+        	cout<<"your cost for standard cab is "<<cost<<endl;
+        	cout<<"press 1 to hire this car "<<endl;
+        	cout<<"or"<<endl;
+        	cout<<"press 2 to select another cab";
+        	cin>>hirecab;
+        	system("cls");
+        	if(hirecab==1){
+        		lastcost=cost;
+        		cout<<"You have successfully hired a standard cab"<<endl;
+        		cout<<"goto menu and take recipt"<<endl;
+			}
+			else if(hirecab==2){
+				cabdetails();
+			}
+			else{
+				cout<<"INVALID INPUT! REDERCATING TO PREIVOUS MENU"<<endl;
+				sleep(999);
+				system("cls")
+				cabdetails();
+			}
+		}
+		}
 	
 };
 class Booking{
